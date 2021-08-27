d8373cdd2720463433af0f3603536e5a86a95e97
DOV3D <davidovelazquez3@gmail.com>
2021-08-27 17:22:14 -0400

    Add self.valid_moves method

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,6 +1,11 @@
 require_relative "./skeleton2/lib/00_tree_node.rb"
 
 class KnightPathFinder
+
+    def self.valid_moves(pos)
+       
+    end
+
     def self.root_node(start_position)
         PolyTreeNode.new(start_position)
     end
@@ -8,6 +13,7 @@ class KnightPathFinder
     def initialize(start_position)
         @root_node = KnightPathFinder.root_node(start_position)
         @path_history = [start_position]
+        @considered_positions 
     end
 
     #   def self.root_node(start_position)
@@ -19,11 +25,17 @@ class KnightPathFinder
     #     @path_history = [start_position]
     # end
 
+    #create an arr called queue 
+    #create until loop until arr is empty>
+    #check to see if the value of our current node is == target
+    #
     def build_move_tree #finding children
+        
     end
 
     def find_path
     end
 
+   
 
 end
