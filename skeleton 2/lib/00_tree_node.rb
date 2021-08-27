8d2130345f4077f6f6f1b6cdf60522e496f5ee1d
DOV3D <davidovelazquez3@gmail.com>
2021-08-27 14:54:15 -0400

    Add add_child and remove_child method

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -22,6 +22,32 @@ class PolyTreeNode
        
     end
 
+    def add_child(child_node)
+        child_node.parent=self
+    end
+
+    def remove_child(child_node)
+        if child_node.parent.nil?
+            raise "No parent"
+        end
+        child_node.parent=nil
+    end
+
+    def dfs(target_value)
+        #check if self.value == target_value return self
+        # iterate through children and call child.dfs(target_value) 
+        #return nil
+        return nil if self.nil?
+        return self if self.value == target_value
+        self.children.each do |child|
+         result = child.dfs(target_value)
+        end
+        return nil
+
+    end
+
+
+
 
 
 end
