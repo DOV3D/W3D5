8b3240a9a2187be5399b614248dd531271d915a0
DOV3D <davidovelazquez3@gmail.com>
2021-08-27 14:08:43 -0400

    Add initialize and parent= method

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+    
+    attr_reader :parent, :children, :value 
+    
+    
+    def initialize(value)
+       @parent= nil
+       @children= []
+       @value = value
+    end
+
+    def parent=(parent_node)
+
+        parent = parent_node.value
+        parent_node.children << self unless parent_node.children.include?(self)
+        
+    end
+
+
 
 end
